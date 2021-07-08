79a3380ced6fcd0ecbde8194dd0be012e7877195
Allan Clempe <allan.clempe@gmail.com>
2021-07-08 12:26:31 +1000

    explosed contracts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { Consumer, ConsumerOptions } from './consumer';
 export { AwsQueueProvider, AzureQueueProvider } from './providers';
+export { Message, IQueueProvider } from './providers/contracts';
+export { SQSError } from './errors';
